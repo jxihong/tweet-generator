86c73871079b83daf723701968c21e675433de17
kklocke <kai.c.klocke@gmail.com>
2016-08-30 22:11:13 -0700

    Update twitter_pull.py for preprocessing

--- twitter_pull.py
+++ twitter_pull.py
@@ -1,4 +1,5 @@
 from twitter import Twitter, OAuth, TwitterHTTPError
+from twitter_preprocess import preprocess
 
 # Set token values
 ACCESS_TOKEN = '586338090-qYKE4AWln63B0nLI8WnNOXwaDZtNgIS1cZJFuq5N'
@@ -8,33 +9,30 @@ CONSUMER_SECRET = 'bTeFv1UCdKgYwAS2nvs6XRaC0NV2zWDXRYC4r94jd40JpmAjIx'
 
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 twitter = Twitter(auth=oauth)
-username = 'dennisnliao'
-numTweets = 20
+username = 'realDonaldTrump'
 
 profile = twitter.users.lookup(screen_name = username)
-numTweets = profile[0]['statuses_count']
+numTweets = min(3200, profile[0]['statuses_count'])
 
-tweets = []
+tweets = twitter.statuses.user_tmeline(screen_name = username, count = 1)
 i = 0
-interval = 100
-id = 537408199532347393
-while(i < numTweets):
-    tweets += twitter.statuses.user_timeline(screen_name = username, count = interval, max_id = id)
-    i += interval
-    currentLen = len(tweets)
-    id = tweets[currentLen - 1]['id']
-print(len(tweets))
+interval = 200
+id = tweets[0]['id']
 
-with open(username + '_tweets.csv', 'w') as f:
-    for tweet in tweets:
-        try:
-            f.write(tweet['created_at'])
-            f.write(',')
-            f.write(tweet['text'])
-            f.write('\n')
-        except:
-            print('FAILED TO WRITE TWEET TO FILE:')
-            print(tweet['created_at'])
-            print(tweet['text'])
-        finally:
-            pass
+while(i < numTweets):
+    myCount = min(interval, numTweets - i)
+    tweets += twitter.statuses.user_timeline(screen_name = username, count = myCount, max_id = id)
+    i += interval
+    id = tweets[myCount - 1]['id'] - 1
+        with open(username + '_tweets.csv', 'w') as f:
+        for tweet in tweets:
+            text = tweet['text']
+            text = preprocess(text)
+            try:
+                f.write(text)
+                f.write('\n')
+            except:
+                print('FAILED TO WRITE TWEET TO FILE:')
+                print(tweet['text'])
+            finally:
+                continue
